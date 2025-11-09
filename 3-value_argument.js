379c3cf8b75a2bbb65debd85900431170f2121da
EDNER ITIRIKIA <ednaitirikia@gmail.com>
2025-11-09 08:45:23 +0300

    Add script to print first argument or No argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,7 @@
-const args = process.argv[2];
+const arg = process.argv[2];
 
-if (args === undefined) {
+if (arg === undefined) {
   console.log("No argument");
 } else {
-  console.log(args);
+  console.log(arg);
 }
