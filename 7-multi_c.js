9f5cbfd86b613f3fe1fc27d5d6b666b9e58192e1
EDNER ITIRIKIA <ednaitirikia@gmail.com>
2025-11-09 09:23:32 +0300

    Fix for correct output of -3

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,10 @@
 const x = parseInt(process.argv[2], 10);
 
-if (!x || x < 1) {
+if (!process.argv[2]) {
   console.log("Missing number of occurrences");
 } else {
   for (let i = 0; i < x; i++) {
     console.log("C is fun");
   }
 }
+
